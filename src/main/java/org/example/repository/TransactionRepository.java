16caf5b16731a87ef1ca7c7dd08c2c573893aa85
Traian-Caescu <32572689+Traian-Caescu@users.noreply.github.com>
2024-11-29 16:38:03 +0000

    Update TransactionRepository.java

--- src/main/java/org/example/repository/TransactionRepository.java
+++ src/main/java/org/example/repository/TransactionRepository.java
@@ -1,7 +1,7 @@
 package org.example.repository;
 
 import org.example.model.Transaction;
-import org.example.model.TransactionStatus;
+import org.example.model.TransactionStatus; 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query; 
 import org.springframework.stereotype.Repository;
